ce5da773fbcacb38e410e141f95968083e475b17
bouncinggift <etukgodsgift@gmail.com>
2022-10-14 16:33:37 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - void
+ * Return: 0
+ */
+int main(void)
+{
+	char x, y;
+
+	for (x = '0'; x <= '9'; x++)
+
+	{
+		putchar(x);
+	}
+
+	for (y = 'a'; y <= 'f'; y++)
+
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
+
