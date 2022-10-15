90b7247d2764dbee567c5a0cca7da31184a15c59
bouncinggift <etukgodsgift@gmail.com>
2022-10-15 05:18:01 -0700

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - void
+ * Return: 0
+ */
+int main(void)
+{
+	int num1, num2;
+	/*ASCII code for 0*/
+	num1 = 48;
+
+	while (num1 < 58)
+
+	{
+		num2 = 49;
+
+		while (num2 < 58)
+
+		{
+			if (num1 != num2 && num2 > num1)
+			{
+				putchar(num1);
+				putchar(num2);
+				putchar(',');
+				putchar(' ');
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
