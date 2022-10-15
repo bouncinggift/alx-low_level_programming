46dd9d1d98817d2d702057ca89d3892fd351b017
bouncinggift <etukgodsgift@gmail.com>
2022-10-15 07:11:42 -0700

    The success combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,47 +1,49 @@
 #include <stdio.h>
 /**
-*main - void
-*Return: 0
-*/
+ * main - viod
+ * Return: 0
+ */
 int main(void)
 {
-    int x, y, z;
-    /*ASCII code for 0*/
-    x = 48;
-    while (x < 58)
-    {
-        y=49;
-        
-        while (y < 58)
-        {
-            z=50;
-            if (x != y && y > x)
-            {
-                while (z < 58)
-                {
-                    if (y != z && z > y)
-                    {
-                        putchar(x);
-                        putchar(y);
-                        putchar(z);
-                        if (x < 55)
-                        {
-                            putchar(',');
-                            putchar(' ');
-                            
-                        }
-                        
-                    }
-                    z++;
-                }
-                
-                
-            }
-            y++;
-            
-        }
-        x++;
-    }
-    putchar('\n');
-    return (0);
+	int x, y, z;
+	/*ASCII code for 0*/
+	x = 48;
+
+	while (x < 58)
+
+	{
+		y = 49;
+
+
+		while (y < 58)
+
+		{
+			z = 50;
+			if (x != y && y > x)
+			{
+
+				while (z < 58)
+
+				{
+					if (y != z && z > y)
+					{
+						putchar(x);
+						putchar(y);
+						putchar(z);
+						if (x < 55)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+					z++;
+				}
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
 }
+
