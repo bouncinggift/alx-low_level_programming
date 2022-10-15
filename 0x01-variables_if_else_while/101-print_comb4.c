046aed74c5b94ae8a8369e4e22741046aa6fdeed
Godsgift Robinson <94372591+bouncinggift@users.noreply.github.com>
2022-10-15 14:36:23 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,47 @@
 #include <stdio.h>
-
+/**
+*main - void
+*Return: 0
+*/
 int main(void)
 {
-	    int x, y, z;
-	        /*ASCII code for 0*/
-	        x = 48;
-		    while (x < 58)
-			        {
-					        y=49;
-						        
-						        while (y < 58)
-								        {
-										            z=50;
-											                if (x != y && y > x)
-														            {
-																                    while (z < 58)
-																			                    {
-																						                        if (y != z && z > y)
-																										                    {
-																													                            putchar(x);
-																																                            putchar(y);
-																																			                            putchar(z);
-																																						                            if (x < 55)
-																																										                            {
-																																														                                putchar(',');
-																																																		                            putchar(' ');
-																																																					                                
-																																																					                            }
-																																									                            
-																																									                        }
-																									                    z++;
-		
+    int x, y, z;
+    /*ASCII code for 0*/
+    x = 48;
+    while (x < 58)
+    {
+        y=49;
+        
+        while (y < 58)
+        {
+            z=50;
+            if (x != y && y > x)
+            {
+                while (z < 58)
+                {
+                    if (y != z && z > y)
+                    {
+                        putchar(x);
+                        putchar(y);
+                        putchar(z);
+                        if (x < 55)
+                        {
+                            putchar(',');
+                            putchar(' ');
+                            
+                        }
+                        
+                    }
+                    z++;
+                }
+                
+                
+            }
+            y++;
+            
+        }
+        x++;
+    }
+    putchar('\n');
+    return (0);
+}
