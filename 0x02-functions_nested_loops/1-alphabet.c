5e41605970d41eeada110e9de3e1195376bedfb6
bouncinggift <etukgodsgift@gmail.com>
2022-10-18 12:55:11 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet(void);
+{
+	char Lc;
+
+	for (Lc = 'a'; Lc <= 'z'; Lc++)
+		_putchar(Lc);
+	_putchar('\n');
+}
