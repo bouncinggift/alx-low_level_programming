9c6cba88bfc2e12fc07115485d7c233ea88ddf06
Godsgift Robinson <94372591+bouncinggift@users.noreply.github.com>
2022-10-19 13:48:05 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: check character
  * Return: 1 if character is letter, 0 otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
