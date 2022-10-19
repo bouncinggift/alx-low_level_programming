333a86a90ec8d7e45e2dee936df6ded712cf29f3
Godsgift Robinson <94372591+bouncinggift@users.noreply.github.com>
2022-10-19 16:37:01 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,18 +8,19 @@ void times_table(void)
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
+		putchar('0');
 
-		for (num2 = 0; num2 <= 9; num2++)
+		for (num2 = 1; num2 <= 9; num2++)
 		{
 			prod = num1 * num2;
+			_putchar(',');
+			_putchar(' ');
 
 			if (prod < 0 && prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
