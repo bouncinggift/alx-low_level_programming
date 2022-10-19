37b68b89084cf90f7a0079b650569340b085575a
bouncinggift <etukgodsgift@gmail.com>
2022-10-19 00:54:43 -0700

    alpha in lowercase nnnbgg

---
+++ 0x02-functions_nested_loops/1_alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lower case
+ */
+void print_alphabet(void);
+{
+	char lc;
+	for (lc = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+	_putchar('\n');
+}
