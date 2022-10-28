ae33c635c48d6b941695356d98b276aa28182818
Godsgift Robinson <94372591+bouncinggift@users.noreply.github.com>
2022-10-28 09:56:26 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,10 +14,10 @@ char *cap_string(char *str)
 			i++;
 		if (str[i - 1] == ' ' ||
 		    str[i - 1] == '\t' ||
-		    str[i -1] == '\n' ||
-		    str[i -1] == ',' ||
-		    str[i -1] == ';' ||
-		    str[i -1] == '?' ||
+		    str[i - 1] == '\n' ||
+		    str[i - 1] == ',' ||
+		    str[i - 1] == ';' ||
+		    str[i - 1] == '?' ||
 		    str[i - 1] == '!' ||
 		    str[i - 1] == '.' ||
 		    str[i - 1] == '"' ||
