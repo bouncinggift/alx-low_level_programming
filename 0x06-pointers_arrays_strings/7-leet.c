bf29f160b096ec1b2b5c20d68eb351758f4909cd
Godsgift Robinson <94372591+bouncinggift@users.noreply.github.com>
2022-10-29 04:41:38 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
  * leet -  encodes a string into 1337
- * @s: input
+ * @n: input
  * Return: s
  */
-char *leet(char *s)
+char *leet(char *n)
 {
 	int i, j;
-	char m1 = "aAeEoOtTlL";
-	char m2 = "4433007711";
+	char s1 = "aAeEoOtTlL";
+	char s2 = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if(s[i] == m1[j])
+			if(n[i] == s1[j])
 			{
-				s[i] = m2[j];
+				n[i] = s2[j];
 			}
 		}
 	}
-	return (s);
+	return (n);
 }
