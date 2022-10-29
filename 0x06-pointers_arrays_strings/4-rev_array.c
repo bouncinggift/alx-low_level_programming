23700d730eb16d56c032e9fd470f3fd366c53a8f
Godsgift Robinson <94372591+bouncinggift@users.noreply.github.com>
2022-10-29 04:25:40 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * reverse_array - reverses the content of an array
@@ -11,6 +10,7 @@ void reverse_array(int *a, int n)
 {
 	int i;
 	int j;
+
 	for (i = 0; i < n; i++)
 	{
 		n--;
